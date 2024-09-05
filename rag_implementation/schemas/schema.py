b93bce4efa140c9ef429382f073a4ad5a60338f2
Sammybams <samuelbamgbola@gmail.com>
2024-09-06 00:55:30 +0100

    Updated serializer

--- rag_implementation/schemas/schema.py
+++ rag_implementation/schemas/schema.py
@@ -3,10 +3,7 @@ def serializer(record):
     return {
         "id": str(record["_id"]),
         "name": record["name"],
+        "email_address": record["email_address"],
         "full_history": record["full_history"],
         "buffer_history": record["buffer_history"]
     }
-
-# def get_last_added_item():
-#     last_item = collection.find().sort("_id", -1).limit(1)
-#     return last_item[0] if last_item.count() > 0 else None
