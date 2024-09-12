978e5a942bdc888c8d11eea8ab67b7f06d18b1b4
khidrhussein <husseinkhidr3@gmail.com>
2024-09-12 07:22:57 +0100

    Fixed the ready function bug in apps.py

--- api/apps.py
+++ api/apps.py
@@ -1,15 +1,13 @@
 from django.apps import AppConfig
 from .scheduler import start
 
-
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
 
     def ready(self):
+        # Import signals
         import api.signals
-
-
-    def ready(self):
+        
         # Start the APScheduler when the Django app is ready
         start()
