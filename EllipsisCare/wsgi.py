9ca382a0ef007753e6b6bed7d0447137921f9051
khidrhussein <husseinkhidr3@gmail.com>
2024-12-12 11:15:47 +0100

    Added module directory in the wsgi file.

--- EllipsisCare/wsgi.py
+++ EllipsisCare/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EllipsisCare.settings')
 
+sys.path.append('C:/Users/Hp/EllipsisCare/EllipsisCare')
+sys.path.append('C:/Users/Hp/EllipsisCare/venv/Lib/site-packages') 
+sys.path.append('/tmp/8dd18639cae188a/requirements.txt')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ellipsiscare.settings')
+
 application = get_wsgi_application()
+
