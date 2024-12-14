354f984a81a24c682c67c70bed17c73be0859d6d
khidrhussein <husseinkhidr3@gmail.com>
2024-12-14 13:57:27 +0100

    Added some requirements for deployment on Azure

--- EllipsisCare/wsgi.py
+++ EllipsisCare/wsgi.py
@@ -1,22 +1,10 @@
-"""
-WSGI config for EllipsisCare project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-import sys
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EllipsisCare.settings')
 
-sys.path.append('C:/Users/Hp/EllipsisCare/venv/Lib/ite-packages')
-sys.path.append('/home/site/wwwroot')
-sys.path.append('C:/Users/Hp/EllipsisCare/venv/Lib/ite-packages/django')
+settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'EllipsisCare.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
 
